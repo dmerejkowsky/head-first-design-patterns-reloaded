17793d73b1bc3dd282a1fe8a96de8fb1e6ada63e
Dimitri Merejkowsky <dimitri@dmerej.info>
2023-07-30 13:09:43 +0200

    Working version with Copy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,6 +2,7 @@
 pub mod barrista;
 mod coffee;
 pub mod home_automation;
+pub mod menus;
 mod pizza;
 mod simuduck;
 mod weather_o_rama;
--- rust/src/menus.rs
+++ rust/src/menus.rs
@@ -1,83 +1,86 @@
-use std::{collections::HashMap, fmt::Debug, io::IntoInnerError};
+use std::collections::HashMap;
 
-trait Menu {
-    fn get_items(&self) -> &[MenuItem];
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct MenuItem {
-    name: String,
     price: u32,
 }
 
 struct PancakesMenu {
     items: [MenuItem; 6],
+    current_pos: usize,
 }
 
-impl Menu for PancakesMenu {
-    fn get_items(&self) -> &[MenuItem] {
-        &self.items
+impl Iterator for PancakesMenu {
+    type Item = MenuItem;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let res = self.items.get(self.current_pos);
+        self.current_pos += 1;
+        res.copied()
     }
 }
 
 struct DinnerMenu {
     items: Vec<MenuItem>,
+    current_pos: usize,
 }
 
-impl Menu for DinnerMenu {
-    fn get_items(&self) -> &[MenuItem] {
-        &self.items
+impl Iterator for DinnerMenu {
+    type Item = MenuItem;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let res = self.items.get(self.current_pos);
+        self.current_pos += 1;
+        res.copied()
     }
 }
 
 struct CoffeeMenu {
     items: HashMap<String, MenuItem>,
+    values: Vec<MenuItem>,
+    current_pos: usize,
 }
 
-struct CoffeeMenuIterator<'a> {
-    values: Vec<&'a MenuItem>,
-}
+impl Iterator for CoffeeMenu {
+    type Item = MenuItem;
 
-impl<'a> CoffeeMenuIterator<'a> {
-    fn new(menu: &'a CoffeeMenu) -> Self {
-        Self {
-            values: menu.items.values().collect(),
-        }
+    fn next(&mut self) -> Option<Self::Item> {
+        let res = self.values.get(self.current_pos);
+        self.current_pos += 1;
+        res.copied()
     }
 }
 
-impl<'a> Iterator for CoffeeMenuIterator<'a> {
-    type Item = &'a MenuItem;
+trait Menu {
+    fn get_items(&self) -> Vec<&MenuItem>;
+}
 
-    fn next(&mut self) -> Option<Self::Item> {
-        self.values.pop()
+impl Menu for PancakesMenu {
+    fn get_items(&self) -> Vec<&MenuItem> {
+        self.items.iter().collect()
+    }
+}
+
+impl Menu for DinnerMenu {
+    fn get_items(&self) -> Vec<&MenuItem> {
+        self.items.iter().collect()
     }
 }
 
 impl Menu for CoffeeMenu {
-    fn get_items(&self) -> &[MenuItem] {
-        let values = self.items.values();
-        let v: Vec<_> = values.into_iter().collect();
-        todo!()
+    fn get_items(&self) -> Vec<&MenuItem> {
+        self.items.values().into_iter().collect()
     }
 }
 
-impl<'a> IntoIterator for CoffeeMenu {
-    type Item = MenuItem;
-
-    type IntoIter<'a> = CoffeeMenuIterator<'a>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        CoffeeMenuIterator::new(&self)
-    }
-}
-
-fn print_menu(menu: &dyn Menu) {
+fn print_menus(menu: &dyn Menu) {
     for item in menu.get_items() {
-        println!("{item:?}");
+        dbg!(item);
     }
 }
 
-fn print_menu2(menu: CoffeeMenu) {
-    for item in menu {}
+fn print_menus2(menu: &mut dyn Iterator<Item = MenuItem>) {
+    for item in menu {
+        dbg!(item);
+    }
 }

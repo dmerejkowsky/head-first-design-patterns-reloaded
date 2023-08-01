e8f7e9b5655fc562419e8be60e39eff144f0474e
Dimitri Merejkowsky <dimitri@dmerej.info>
2023-08-01 12:41:49 +0200

    rust: version with Rc

--- rust/src/menus.rs
+++ rust/src/menus.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 #[derive(Debug)]
 struct MenuItem {
     name: &'static str,
@@ -26,11 +28,11 @@ impl Component {
 
 struct Menu {
     name: &'static str,
-    components: Vec<Component>,
+    components: Vec<Rc<Component>>,
 }
 
 impl Menu {
-    fn new(name: &'static str, components: Vec<Component>) -> Self {
+    fn new(name: &'static str, components: Vec<Rc<Component>>) -> Self {
         Self { name, components }
     }
 
@@ -43,11 +45,21 @@ impl Menu {
 }
 
 pub fn run() {
-    let pasta = Component::Item(MenuItem::new("pasta", 4));
-    let waffles = Component::Item(MenuItem::new("waffles", 2));
-    let apple_pie = Component::Item(MenuItem::new("Apple pie", 3));
-    let diner = Menu::new("Diner", vec![pasta, waffles, apple_pie]);
-    diner.print();
+    let pasta = Rc::new(Component::Item(MenuItem::new("pasta", 4)));
+    let waffles = Rc::new(Component::Item(MenuItem::new("waffles", 2)));
+    let apple_pie = Rc::new(Component::Item(MenuItem::new("Apple pie", 3)));
+    let diner = Component::Menu(Menu::new(
+        "Diner",
+        vec![
+            Rc::clone(&pasta),
+            Rc::clone(&waffles),
+            Rc::clone(&apple_pie),
+        ],
+    ));
 
-    let breakfast = Menu::new("Breakfast", vec![waffles]);
+    let breakfast = Component::Menu(Menu::new("Breakfast", vec![Rc::clone(&waffles)]));
+
+    let all_menus = Menu::new("All menus", vec![Rc::new(diner), Rc::new(breakfast)]);
+
+    all_menus.print();
 }
